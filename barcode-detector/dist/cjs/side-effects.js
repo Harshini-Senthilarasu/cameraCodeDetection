620a90edd37fdca954afd33ef1ad6f12ce1ad32f
Harshini-Senthilarasu <91965110+Harshini-Senthilarasu@users.noreply.github.com>
2024-04-05 14:22:54 +0800

    Update side-effects.js

--- barcode-detector/dist/cjs/side-effects.js
+++ barcode-detector/dist/cjs/side-effects.js
@@ -1,1 +1,10 @@
+export { setZXingModuleOverrides } from "./BarcodeDetector.js";
+declare global {
+    var BarcodeDetector: typeof import("./BarcodeDetector.js").BarcodeDetector;
+    type BarcodeDetector = import("./BarcodeDetector.js").BarcodeDetector;
+    type BarcodeFormat = import("./BarcodeDetector.js").BarcodeFormat;
+    type BarcodeDetectorOptions = import("./BarcodeDetector.js").BarcodeDetectorOptions;
+    type DetectedBarcode = import("./BarcodeDetector.js").DetectedBarcode;
+}
+
 "use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});const r=require("./pure.js");var e;(e=globalThis.BarcodeDetector)!=null||(globalThis.BarcodeDetector=r.BarcodeDetector);exports.setZXingModuleOverrides=r.setZXingModuleOverrides;
