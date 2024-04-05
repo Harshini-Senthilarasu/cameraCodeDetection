95cf4a7539987a1928d702825199b72f68fbf7ee
Harshini-Senthilarasu <91965110+Harshini-Senthilarasu@users.noreply.github.com>
2024-04-05 14:21:16 +0800

    Update index.js

--- barcode-detector/dist/cjs/index.js
+++ barcode-detector/dist/cjs/index.js
@@ -1,3 +1,3 @@
-import { BarcodeDetector } from './pure.js';
 import './side-effects.js';
+export * from './pure.js';
 "use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});require("./side-effects.js");const e=require("./pure.js");exports.BarcodeDetector=e.BarcodeDetector;exports.setZXingModuleOverrides=e.setZXingModuleOverrides;
