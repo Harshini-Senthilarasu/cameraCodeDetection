5c9cc6789efd0740ef34608d4dbcd145fa20c928
Harshini-Senthilarasu <91965110+Harshini-Senthilarasu@users.noreply.github.com>
2024-04-11 13:18:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,13 +44,13 @@ var tableData = [{  codeValue: "4902179022226",
                     itemName: "Lemon Tea", 
                     requiredQty: 1,
                     scannedQty: 0},
-                {   codeValue: "4901085613580", 
-                    itemName: "Tully's Coffee Drink", 
-                    requiredQty: 3,
-                    scannedQty: 0},
+                // {   codeValue: "4901085613580", 
+                //     itemName: "Tully's Coffee Drink", 
+                //     requiredQty: 3,
+                //     scannedQty: 0},
                 // {   codeValue: "4901306069530", 
                 //     itemName: "Wheat Drink", 
-                //     requiredQty: 2,
+                //     requiredQty: 3,
                 //     scannedQty: 0},
 ];
 
@@ -59,16 +59,73 @@ Brief:          This function would display the tableData array
                 into the html page under thr inventoryTable division
 Parameters:
 */
-function displayTable() {
+function displayTable(val, rowIndex) {
     tableBody.innerHTML = ''; // intialise table element
+    var arrayIndex = 0;
     tableData.forEach(function(row) {
+        if ((val != 3) && (row.scannedQty > row.requiredQty)) {
+            row.scannedQty--;
+            exceededPopup.style.display = 'block';
+        }
         var tr = document.createElement('tr'); //create row element in the table body
         tr.innerHTML =  "<td>" + row.codeValue + "</td>" +
                         "<td>" + row.itemName + "</td>" +
                         "<td>" + row.requiredQty + "</td>" +
                         "<td>" + row.scannedQty + "</td>";
+
+        if (rowIndex === arrayIndex) {
+            if (row.scannedQty === row.requiredQty) {val = 2;}
+            console.log(val);
+            switch(val) {
+                case 0:
+                    //do nothing
+                    break;
+                case 1: //if there is an incremeent in the value
+                    tr.style.backgroundColor = 'lightgreen';
+                    val = 0;
+                    break;
+                case 2: // if the scanned qty meets the required qty
+                    tr.style.backgroundColor = 'darkgreen';
+                    tr.style.color = 'white';
+                    val = 0;
+                    break;
+                case 3: // when a new item has been added
+                    tr.style.backgroundColor = 'turquoise';
+                    val = 0;
+                    break;
+                case 4: // when required amount has been changed
+                    tr.style.backgroundColor = 'lightblue';
+                    val = 0;
+                    break;
+            }
+        }
+
+        if(row.scannedQty === row.requiredQty){
+            tr.style.backgroundColor = 'darkgreen';
+            tr.style.color = 'white';
+        }
+        // else if(row.scannedQty > row.requiredQty) {
+        //     tr.style.backgroundColor = 'red';
+        //     tr.style.color = 'white';
+        // }
+
         tableBody.appendChild(tr); // each row in the array would be added into the table in the html
+        arrayIndex++;
     })    
+
+    //check if all items have been added
+    var completedList = 0; //value to increment if the item has been completed
+    var numOfItems = tableData.length; //find total number of rows in the data
+    console.log('Reached');
+    tableData.forEach(function(row) {
+        if(row.scannedQty === row.requiredQty) {
+            completedList++;
+            console.log(completedList);
+        }
+        if(completedList === numOfItems) {
+            completedPopup.style.display = 'block';
+        }
+    })
 }
 
 /*
@@ -88,7 +145,7 @@ async function cameraInit() {
             video.srcObject = stream;
             video.onloadedmetadata = function() {
                 video.play();
-                displayTable(); //display of table 
+                displayTable(0, 0); //display of table 
                 startScanning();
             };
         })
@@ -128,7 +185,9 @@ function startScanning() {
                         const existingRowIndex = tableData.findIndex(row => row.codeValue === barcode.rawValue); //find row index in tabledata that matches with barcode value
                         if (existingRow) {
                             existingRow.scannedQty++; //Increment the quantity of the item in that row in tableData
-                            displayTable(); // display changed value
+                            console.log('increment:',existingRow.requiredQty);
+                            console.log(existingRow.scannedQty);
+                            displayTable(1, existingRowIndex); // display changed value
                             result.innerHTML = `<p>${existingRow.itemName} has been incremented.<p>`; // display result in html
                         }
                         else { // if item does not exist in tableData, open up newScanPopup
@@ -210,12 +269,13 @@ function confirmButton(event) {
                 var rowData = {};
                 rowData.codeValue = newValue.value;
                 rowData.itemName = newItemName.value;
-                rowData.requiredQty = reqQty.value;
-                rowData.scannedQty = scanQty.value;
+                rowData.requiredQty = parseInt(reqQty.value);
+                rowData.scannedQty = parseInt(scanQty.value);
+                console.log('New scan:',rowData.requiredQty);
                 tableData.push(rowData); // add row into tableData
 
                 const rowIndex = tableData.findIndex(row =>row.codeValue === rowData.codeValue);
-                displayTable();
+                displayTable(3, rowIndex);
                 newScanPopup.style.display = 'none';
                 result.innerHTML = `<p>${rowData.itemName} has been added.<p>`;
             }
@@ -226,20 +286,16 @@ function confirmButton(event) {
     }
 
     else if(parentPopup.id === "reqQtyPopup") {
-        if (newReqQty) {
-          if (confirm('Are you sure you want to edit the item?')) {
-              const dropDownRow = tableData.find(row => row.itemName === reqDropDown.value);
-              dropDownRow.requiredQty = parseInt(newReqQty.value); 
-              const dropDownIndex = tableData.findIndex(row => row.itemName === reqDropDown.value);
-              displayTable();
-              reqDropDown.value = '';
-              reqQtyPopup.style.display = 'none';
-              result.innerHTML = `<p> Required quantity of ${dropDownRow.itemName} has been changed.<p>`;
-              scanBtn.style.display  = 'block'; //display scan button
-          }
-        }
-        else {
-          alert("Please enter all required values.");
+        if (confirm('Are you sure you want to edit the item?')) {
+            const dropDownRow = tableData.find(row => row.itemName === reqDropDown.value);
+            dropDownRow.requiredQty = parseInt(newReqQty.value); 
+            console.log('Required edit:',dropDownRow.requiredQty);
+            const dropDownIndex = tableData.findIndex(row => row.itemName === reqDropDown.value);
+            displayTable(4, dropDownIndex);
+            reqDropDown.value = '';
+            reqQtyPopup.style.display = 'none';
+            result.innerHTML = `<p> Required quantity of ${dropDownRow.itemName} has been changed.<p>`;
+            scanBtn.style.display = 'block';
         }
     }
     else if (parentPopup.id === "rmvPopup") {
@@ -249,10 +305,10 @@ function confirmButton(event) {
             if (dropDownIndex !== -1) {
                 tableData.splice(dropDownIndex, 1);
             }
-            displayTable();
+            displayTable(0, 0);
             rmvPopup.style.display = 'none';
             result.innerHTML = `<p> ${dropDownRow.itemName} has been removed.<p>`; 
-            scanBtn.style.display  = 'block'; //display scan button
+            scanBtn.style.display = 'block';
         }
     }
 }
